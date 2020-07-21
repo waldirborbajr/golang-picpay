f79f58a5c050b0d3fb2ed85c9a53367a32b04677
Marcus <mpereirassa@gmail.com>
2020-07-21 10:48:23 -0300

    .fix: test

--- downloader/downloader_test.go
+++ downloader/downloader_test.go
@@ -1,15 +1,20 @@
-package downloader
+package downloader_test
 
 import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
+	"github.com/marcuxyz/golang-picpay/downloader"
+	"github.com/marcuxyz/golang-picpay/response"
+
+	picpay "github.com/marcuxyz/golang-picpay"
 	"github.com/stretchr/testify/assert"
 )
 
-var status = StatusResultJSON{
+var status = response.StatusResponse{
 	AuthorizationID: "123",
 	ReferenceID:     "1203",
 	Status:          "paid",
@@ -19,12 +24,14 @@ func TestMakeDownloaderStatusCode200(t *testing.T) {
 	server := createServer()
 	defer server.Close()
 
-	picpay := Picpay{
-		Token:   "",
-		BaseURL: server.URL,
+	URL, _ := url.Parse(server.URL)
+
+	picpay := picpay.Picpay{
+		Token: "",
+		URL:   URL,
 	}
 
-	resp, err := MakeDownloader(http.MethodGet, server.URL, picpay.Token, nil)
+	resp, err := downloader.MakeDownloader(http.MethodGet, server.URL, picpay.Token, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
